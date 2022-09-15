ee9ee4b38540eed9468343df2811f01798771ea9
Соня <lisofang@yandex.ru>
2022-09-15 19:15:21 +0300

    initial commit

---
+++ HomeWork/Program.cs
@@ -0,0 +1,26 @@
+﻿double distance = 10000;
+int count = 0;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+double temp = 0;
+
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        temp = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else 
+    {
+        temp = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+distance = distance - (firstFriendSpeed + secondFriendSpeed) * temp;
+count = count + 1;
+}
+Console.WriteLine(count);
+
